4723e0d506b92ae1cbc05d307153f4d3cde87bf5
Ang Panha <angpanha2019@gmail.com>
2025-11-21 11:41:48 +0700

    11212025-transfer to laptop

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import CategorySection from '@/components/section/CategorySection.vue';
-import PromoSection from '@/components/section/PromoSection.vue';
+import CategorySection from '@/components/section/CategorySection.vue'
+import PromoSection from '@/components/section/PromoSection.vue'
 </script>
 
 <template>
-  <CategorySection class="quicksand" />
-  <PromoSection class="quicksand" />
+  <div class="center-items app">
+    <CategorySection class="quicksand" />
+    <PromoSection class="quicksand" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
--- src/components/shop/PromoCard.vue
+++ src/components/shop/PromoCard.vue
@@ -1,26 +1,40 @@
 <template>
   <UiCard class="promocard">
     <div class="promo-desc">
-      <h1 class="promo-label">{{ label }}</h1>
-      <UiButton :label="`Shop Now &rarr;`"></UiButton>
+      <h5 class="promo-label">{{ label }}</h5>
+      <UiButton class="quicksand" :label="`Shop Now &rarr;`"></UiButton>
     </div>
     <div class="img-wrapper">
-      <img :src="imgPath" :alt="imgPath">
+      <img class="promo-img" :src="imgPath" :alt="imgPath" />
     </div>
   </UiCard>
 </template>
 
 <script setup lang="ts">
-import UiCard from '@/components/ui/UiCard.vue';
-import UiButton from '@/components/ui/UiButton.vue';
-import { defineProps } from 'vue';
+import UiCard from '@/components/ui/UiCard.vue'
+import UiButton from '@/components/ui/UiButton.vue'
+import { defineProps } from 'vue'
 
 defineProps<{
-  label: string,
+  label: string
   imgPath: string
-}>();
+}>()
 </script>
-
 <style scoped>
-
+.promocard {
+  height: 250px;
+  width: 600px;
+  border-radius: 20px;
+}
+.img-wrapper {
+  background-color: whitesmoke;
+  height: 100%;
+  overflow: hidden;
+  padding: 20px;
+}
+.promo-img {
+  object-fit: cover;
+  height: 75%;
+  translate: 12% 10%;
+}
 </style>

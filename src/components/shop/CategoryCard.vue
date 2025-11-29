46ae362156a02381e8cf4144e4a5be1b0b0b52d3
Ang Panha <angpanha2019@gmail.com>
2025-11-29 22:43:29 +0700

    tp1

--- src/components/shop/CategoryCard.vue
+++ src/components/shop/CategoryCard.vue
@@ -1,82 +1,106 @@
 <template>
   <UiCard
-  class="category-card center-items"
-  :class="{ highlighted: isHovered }"
-  :style="{ backgroundColor: bgColor, outline: `${isHovered ? 2 : 0}px solid ${outlineColor}`}"
-  @mouseover="isHovered = true"
-  @mouseleave="isHovered = false"
+    class="card center-items"
+    :class="{ highlighted: isHovered }"
+    :style="{ backgroundColor: bgColor, outline: `${isHovered ? 2 : 0}px solid ${outlineColor}` }"
+    @mouseover="isHovered = true"
+    @mouseleave="isHovered = false"
   >
-    <div class="img-wrapper center-items">
-      <img
-      ref="cardImage"
-      class="card-img"
-      :src="imgPath"
-      :alt="cardLabel"
-      @load="onImageLoad"
-      >
+    <!-- image container and content -->
+    <div class="img-box center-items">
+      <img ref="img" class="img" :src="imgPath" :alt="cardLabel" @load="onImageLoad" />
     </div>
-    <div class="card-desc center-items">
-      <p class="card-label" :style="{ color: labelColor }">{{ cardLabel }}</p>
-      <p class="card-sublabel" :style="{ color: subLabelColor }">{{ cardSubLabel }}</p>
+    <!-- description container and content -->
+    <div class="desc center-items">
+      <p class="label" :style="{ color: labelColor }">{{ cardLabel }}</p>
+      <p class="sublabel" :style="{ color: subLabelColor }">{{ cardSubLabel }}</p>
     </div>
   </UiCard>
 </template>
 
 <script setup lang="ts">
-  import {defineProps, ref} from 'vue';
-  import UiCard from '@/components/ui/UiCard.vue';
-  import '@/assets/main.css';
-  import useDominantColor from '@/composables/useDominantColor.ts';
-  defineProps<{
-    imgPath?: string,
-    cardLabel?: string,
-    cardSubLabel?: string
-  }>();
-  const isHovered = ref(false);
-  const { bgColor, outlineColor, labelColor, subLabelColor, getDominantColor } = useDominantColor();
-  const cardImage = ref(null);
-  const onImageLoad = () => {
-    getDominantColor(cardImage.value);
-  }
+import { defineProps, ref } from 'vue'
+import '@/assets/main.css'
+import UiCard from '@/components/ui/UiCard.vue'
+import useDominantColor from '@/composables/useDominantColor.ts'
+
+// properties
+defineProps<{
+  imgPath?: string
+  cardLabel?: string
+  cardSubLabel?: string
+}>()
+
+// states
+const isHovered = ref(false)
+
+// element references
+const img = ref(null)
+
+// composables
+const { bgColor, outlineColor, labelColor, subLabelColor, getDominantColor } = useDominantColor()
+
+// events
+const onImageLoad = () => getDominantColor(img.value)
 </script>
 
 <style scoped>
-  /* .category-card, .img-wrapper, .card-img, .card-desc, .card-label, .card-sublabel {
-    border: 1px solid blue;
-  } */
-  .category-card {
-    width: 100px;
-    aspect-ratio: 3 / 4;
-    gap: 5%;
-    flex-direction: column;
-    background-color: #f5f5f5;
-  }
-  .highlighted {
-    box-shadow: 10px 10px 20px 0px #18181824;
-  }
-  .img-wrapper {
-    height: 40%;
-    width: 100%;
-    overflow: hidden;
-  }
-  .card-img {
-    object-fit: cover;
-    height: 100%;
-    width: 60%;
-  }
-  .card-desc {
-    width: 100%;
-    flex-direction: column;
-    text-align: center;
-    gap: 5px;
-  }
-  .card-label {
-    font-weight: bold;
-    font-size: 0.75em;
-  }
-  .card-sublabel {
-    color: gray;
-    font-size: 0.6em;
-  }
-  p { margin: 0 }
+.card {
+  /* dimension */
+  width: 100px;
+  aspect-ratio: 3 / 4;
+
+  /* color */
+  background-color: #f5f5f5;
+
+  /* item structure */
+  gap: 5%;
+  flex-direction: column;
+}
+
+.highlighted {
+  box-shadow: 10px 10px 20px 0px #18181824;
+}
+
+.img-box {
+  /* dimension */
+  height: 40%;
+  width: 100%;
+
+  /* behavior */
+  overflow: hidden;
+}
+
+.desc {
+  /* dimension */
+  width: 100%;
+
+  /* item structure */
+  flex-direction: column;
+  text-align: center;
+  gap: 5px;
+}
+
+.label {
+  font-weight: bold;
+  font-size: 0.75em;
+}
+
+.sublabel {
+  color: gray;
+  font-size: 0.6em;
+}
+
+img {
+  /* dimension */
+  height: 100%;
+  width: 60%;
+
+  /* behavior */
+  object-fit: cover;
+}
+
+p {
+  margin: 0;
+}
 </style>
--- src/components/shop/PromoCard.vue
+++ src/components/shop/PromoCard.vue
@@ -1,40 +1,60 @@
 <template>
-  <UiCard class="promocard">
+  <UiCard class="card">
     <div class="promo-desc">
       <h5 class="promo-label">{{ label }}</h5>
       <UiButton class="quicksand" :label="`Shop Now &rarr;`"></UiButton>
     </div>
-    <div class="img-wrapper">
-      <img class="promo-img" :src="imgPath" :alt="imgPath" />
+    <div class="img-box">
+      <img ref="img" class="img" :src="imgPath" :alt="imgPath" />
     </div>
   </UiCard>
 </template>
 
 <script setup lang="ts">
+import { defineProps } from 'vue'
 import UiCard from '@/components/ui/UiCard.vue'
 import UiButton from '@/components/ui/UiButton.vue'
-import { defineProps } from 'vue'
 
+// properties
 defineProps<{
   label: string
   imgPath: string
 }>()
 </script>
+
 <style scoped>
-.promocard {
+.card {
+  /* dimension */
   height: 250px;
   width: 600px;
+
+  /* style */
   border-radius: 20px;
-}
-.img-wrapper {
+
+  /* color */
   background-color: whitesmoke;
+}
+.img-box {
+  /* color */
+  background-color: whitesmoke;
+
+  /* dimension */
   height: 100%;
+
+  /* behavior */
   overflow: hidden;
+
+  /* style */
   padding: 20px;
 }
-.promo-img {
-  object-fit: cover;
+.img {
+  /* dimension */
   height: 75%;
+
+  /* position */
   translate: 12% 10%;
+
+  /* behavior */
+  object-fit: cover;
 }
 </style>
